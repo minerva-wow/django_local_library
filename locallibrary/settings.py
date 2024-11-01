8c5b061b2c607a2b15d1b66d8cb0c5bb3e4d592f
minerva wow <134083048+minerva-wow@users.noreply.github.com>
2024-11-01 21:17:49 +0800

    Update settings.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -35,8 +35,10 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-&psk#na5l=p3q8
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['andreawow.pythonanywhere.com', '127.0.0.1']
+# During development, you can instead set just the base URL
+# (you might decide to change the site a few times).
+# ALLOWED_HOSTS = ['.pythonanywhere.com','127.0.0.1']
 
 # Application definition
 
@@ -162,3 +164,8 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
+CSRF_TRUSTED_ORIGINS = ['https://andreawow.pythonanywhere.com']
+
+# During development/for this tutorial you can instead set just the base URL
+# CSRF_TRUSTED_ORIGINS = ['https://*.pythonanywhere.com']
+
